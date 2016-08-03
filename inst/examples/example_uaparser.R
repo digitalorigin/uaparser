848cf336a61a8d1662018c6506f16b32ff038b93
Roc Alabern <roc.alabern@gmail.com>
2016-08-03 19:10:59 +0200

    .

--- inst/examples/example_uaparser.R
+++ inst/examples/example_uaparser.R
@@ -1,3 +1,4 @@
+library(uaparser)
 
 r_string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 output = parse_user_agent(r_string)
